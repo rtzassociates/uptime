af1e1705dece9b5d2e2ba32afef6b66c4c9a1330
James Gifford <james@hyperbole.local>
2013-01-02 14:57:40 -0800

    basic services and status

---
+++ app/models/service.rb
@@ -0,0 +1,5 @@
+class Service < ActiveRecord::Base
+  attr_accessible :name, :status_id
+  
+  belongs_to :status
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uptime::Application.config.secret_token = '3d6c0441bb45d4bec5d131ee6b87b5cec28b06843824b7903a5041e2e8b1e4abc12cff013339ff56a51c0542cf84caf547ed340a94e612eb41982e7138c2f55b'
