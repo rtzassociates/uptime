52abcb0b3ddbb8a4ba5ac618996151f3dd5f2e6c
James Gifford <james@trundle.local>
2013-01-12 11:01:03 -0800

    fixed a bug where events class needs to be reloaded when adding a new status type

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,7 @@
 class Status < ActiveRecord::Base
   attr_accessible :value, :foreground_color, :background_color
+  
+  after_save :reload_events
     
   has_many :events
   
@@ -9,6 +11,10 @@ class Status < ActiveRecord::Base
   
   def self.colors
     File.read(Rails.root.join('app/assets/colors.txt')).split("\n")
+  end
+  
+  def reload_events
+    load 'event.rb'
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130109213019) do
+ActiveRecord::Schema.define(:version => 20130112025138) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -86,6 +86,9 @@ ActiveRecord::Schema.define(:version => 20130109213019) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "role"
+    t.string   "first"
+    t.string   "last"
+    t.string   "phone"
   end
 
 end
