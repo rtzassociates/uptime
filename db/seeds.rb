ed8f8bbabd9575240f0816eb77bca23fe3f96135
James Gifford <james@hyperbole.getcare.com>
2013-01-18 10:17:56 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-statuses = Status.create([{:value => 'outage', :foreground_color => "white", :background_color => "red"}])
-            
+statuses = Status.create([{:value => 'outage', :foreground_color => "white", :background_color => "red"}])           
 users = User.create([{:username => "james", :password => "rasputin"}])
 
-services = Service.create([{:name => "cagetcare"},
-                           {:name => "sfgetcare"},
-                           {:name => "msgetcare"}])
             
