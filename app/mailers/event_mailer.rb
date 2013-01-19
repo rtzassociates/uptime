0dbee9b41f41f2b2f77c2cf34695ff239510d63c
James Gifford <james@hyperbole.getcare.com>
2013-01-18 16:23:27 -0800

    javascript

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,6 +2,7 @@ class EventMailer < ActionMailer::Base
   default :from => "mailer@uptime.getcare.com"
 
   def event_notification(event)
+    @event = event
     services = event.services.map { |s| s.name }.join(", ")
     mail(:to => event.recipients, :subject => "#{event.status.value.upcase} -- #{services}")
   end
