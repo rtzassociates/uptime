3b34a6d6f55f9e217d7a01d1d5df5ea47ad40abb
james gifford <james@getcare.com>
2013-03-15 11:10:27 -0700

    updated mail host address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ Uptime::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { host: "uptime.rtzassociates.com" }
+  config.action_mailer.default_url_options = { host: "uptime.getcare.com" }
 end
