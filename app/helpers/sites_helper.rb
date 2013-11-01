a6bedaa227ce627a5ae7eb2854f9d0d73af7bbdb
james gifford <james@getcare.com>
2013-11-01 09:21:11 -0700

    can delete users

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,7 +1,7 @@
 module SitesHelper
   def users_subscribed_to(site)
     unless site.users.empty?
-      subscription_list = site.users.collect { |u| link_to(u.username, user_path(u), :class => "link-large") }.to_sentence
+      subscription_list = site.users.active.collect { |u| link_to(u.username, user_path(u), :class => "link-large") }.to_sentence
       raw "#{subscription_list} #{pluralize_subscription_list(site)} subscribed to this site"
     end
   end
