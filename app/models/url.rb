93615219ec837ebc601643a6842ef21f5b6781a8
james gifford <james@getcare.com>
2013-11-05 14:29:11 -0800

    database fragment cacheing

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ActiveRecord::Base
   attr_accessible :name
   
-  belongs_to :site
+  belongs_to :site, touch: true
   
   validates_format_of :name, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 end
