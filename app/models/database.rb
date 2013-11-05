d57da1a7d19f69dd03ec265057ad19478f7991df
james gifford <james@getcare.com>
2013-11-05 15:00:08 -0800

    database fragment cacheing

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,7 +1,7 @@
 class Database < ActiveRecord::Base
   attr_accessible :name, :database_server_id, :site_ids
   
-  belongs_to :database_server
+  belongs_to :database_server, touch: true
   
   has_many :site_databases, :dependent => :destroy
   has_many :sites, :through => :site_databases
