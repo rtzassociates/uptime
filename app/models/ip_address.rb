3be0bb8934383b23c60f75b0ce89c46c3db73ade
james gifford <james@getcare.com>
2013-11-05 12:41:19 -0800

    ip address validations

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -4,7 +4,7 @@ class IpAddress < ActiveRecord::Base
   belongs_to :network
   belongs_to :server
   
-  ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+  ValidIpAddressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
   validates_format_of :value, :with => ValidIpAddressRegex
   validates :value, :presence => true, :uniqueness => true
   validates :network_id, :presence => true
