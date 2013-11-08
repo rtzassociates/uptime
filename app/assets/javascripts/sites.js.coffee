15f2b9ef0dcec109863b17344a7505cb4e2d2f65
james gifford <james@getcare.com>
2013-11-08 15:51:10 -0800

    database pagination

--- app/assets/javascripts/sites.js.coffee
+++ app/assets/javascripts/sites.js.coffee
@@ -2,3 +2,7 @@ jQuery ->
   $('#site_user_ids').chosen()
   $('#site_application_server_ids').chosen()
   $('#site_database_ids').chosen()
+  $('#sites').dataTable
+    "bPaginate": false,
+    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+    "sPaginationType": "bootstrap"
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,23 +2,10 @@ class SitesController < ApplicationController
   def show
     @site = Site.find(params[:id])
     @events = @site.events.page(params[:page]).per_page(5)
-    
-    unless params[:start_date] == ''
-      @start_date = params[:start_date] ? params[:start_date].to_date : 1.week.ago.to_date
-    else
-      @start_date = 1.week.ago.to_date
-    end
-  
-    unless params[:end_date] == ''
-      @end_date = params[:end_date] ? params[:end_date].to_date : Date.today
-    else
-      @end_date = Date.today
-    end
-    
   end
 
   def index
-    @sites = Site.search(params[:search]).page(params[:page]).per_page(50)
+    @sites = Site.order(:name)
   end
 
   def new
@@ -32,7 +19,7 @@ class SitesController < ApplicationController
   def create
     @site = Site.new(params[:site])
     if @site.save
-      redirect_to @site, :notice => 'Site successfully created'
+      redirect_to @site, :notice => 'Site successfully created.'
     else
       render 'new'
     end
@@ -44,7 +31,7 @@ class SitesController < ApplicationController
     if @site.update_attributes(params[:site])
       expire_fragment @site
       
-      redirect_to @site, :notice => 'Site successfully updated'
+      redirect_to @site, :notice => 'Site successfully updated.'
     else
       render 'edit'
     end
@@ -53,6 +40,6 @@ class SitesController < ApplicationController
   def destroy
     @site = Site.find(params[:id])
     @site.destroy
-    redirect_to sites_path, :notice => 'Site successfully destroyed'
+    redirect_to sites_path, :notice => 'Site successfully destroyed.'
   end
 end
