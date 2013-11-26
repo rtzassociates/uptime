f2d846c7c25d1621e217b46964a5af5cf2f7adb5
charmlesss <james@hyperbole.local>
2013-11-26 13:45:29 -0800

    removed interfaces

--- app/models/generic_server.rb
+++ app/models/generic_server.rb
@@ -1,3 +1,9 @@
 class GenericServer < Server
+  attr_accessor :new_server_role_name
+  before_save :create_server_role_from_name
   belongs_to :server_role
+
+  def create_server_role_from_name
+    create_server_role(:name => new_server_role_name) unless new_server_role_name.blank?
+  end
 end
--- app/models/interface.rb
+++
@@ -1,5 +0,0 @@
-class Interface < ActiveRecord::Base
-  attr_accessible :name
-  
-  has_many :ip_addresses
-end
